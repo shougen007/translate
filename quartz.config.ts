8fe69921ed389b1dcdef69d10051c7468505cb78
sanghyun <son@sanghyuns-Mac-mini.local>
2024-05-12 19:22:50 +0900

    Quartz sync: May 12, 2024, 7:22 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Converging",
+    pageTitle: "🪴 Saloo",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "saloo.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
